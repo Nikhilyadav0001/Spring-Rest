70fb0f0b04eddd8b0326017788ab95b928a97e00
nikhilyadav0001 <nikhilraoshab1@gmail.com>
2026-04-09 01:13:09 +0530

    @Pathvarivale

--- 06-springPathVariableApp/src/main/java/nikhil/rest/ProductRestController.java
+++ 06-springPathVariableApp/src/main/java/nikhil/rest/ProductRestController.java
@@ -12,23 +12,46 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductRestController {
 
 	
+	/*
+	 * R.T    : ResponseEntity<String>
+	 * METHOD : GET
+	 * PATH   : /get -> static path , {pid}/{pname} -> dynamic path
+	 * INPUT  : No input
+	 * 
+	 */
+	
 	@GetMapping("/get/{pid}/{pname}")
-	public ResponseEntity<String> getProductDetails(
-					@PathVariable	Integer pid,
-					@PathVariable   String pname
-			) {
-	//	new ResponseEntity<String>("PID="+pid+"Pname="+pname,HttpStatus.OK)
-		String str = "PID="+pid+"Pname="+pname;
-		return ResponseEntity.ok(str);
+	public ResponseEntity<String> getProductDetails
+		(
+			@PathVariable  Integer pid,
+			@PathVariable  String pname
+		) {
+		
+		//new ResponseEntity<String>("PID = "+pid + " : "+"PNAME = "+pname,HttpStatus.OK);
+		
+		String body = "PID = "+pid + " : "+"PNAME = "+pname;
+		return ResponseEntity.ok(body);
 	}
+	
+	
+	/*
+	 * R.T    : ResponseEntity<String>
+	 * METHOD : find
+	 * PATH   : /get -> static path 
+	 * INPUT  : @RequestParam (pid,pname)
+	 * 
+	 */
 	@GetMapping("/find")
-	public ResponseEntity<String> getProductInfo(
-			@RequestParam	Integer pid,
-			@RequestParam 	String pname
-			) {
-		//	new ResponseEntity<String>("PID="+pid+"Pname="+pname,HttpStatus.OK)
-		String str = "PID="+pid+"Pname="+pname;
-		return ResponseEntity.ok(str);
+	public ResponseEntity<String> getProductInfo
+		(
+			@RequestParam  Integer pid,
+			@RequestParam  String pname
+		) {
+		
+		//new ResponseEntity<String>("PID = "+pid + " : "+"PNAME = "+pname,HttpStatus.OK);
+		
+		String body = "PID = "+pid + " : "+"PNAME = "+pname;
+		return ResponseEntity.ok(body);
 	}
 	
 }
