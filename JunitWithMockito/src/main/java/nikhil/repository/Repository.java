3ba23e26c2c9b2adbf021b5824a823921e7a358f
nikhilyadav0001 <nikhilraoshab1@gmail.com>
2026-04-14 05:10:44 +0530

    Junit with mockito basis

---
+++ JunitWithMockito/src/main/java/nikhil/repository/Repository.java
@@ -0,0 +1,12 @@
+package nikhil.repository;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class Repository {
+	
+	//implementation is not ready
+	public List<String> getData() throws SQLException {
+		return null;
+	}
+}
