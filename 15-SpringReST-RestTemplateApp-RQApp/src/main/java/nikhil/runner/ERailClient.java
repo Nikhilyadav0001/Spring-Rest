f1d710c232054f3e28fed36f806b9d74e5c72c31
nikhilyadav0001 <nikhilraoshab1@gmail.com>
2026-04-15 11:30:54 +0530

    R.t code of synchronisation

---
+++ 15-SpringReST-RestTemplateApp-RQApp/src/main/java/nikhil/runner/ERailClient.java
@@ -0,0 +1,60 @@
+package nikhil.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import nikhil.request.PassengerInfo;
+import nikhil.response.Ticket;
+
+@Component
+public class ERailClient implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		//REQUEST URL
+		String REST_ENDPOINT_URL="http://localhost:8888/v1/api/railway/createTicket";
+		
+		// Sending the request object[Passenger object]
+		PassengerInfo pinfo = new PassengerInfo();
+		pinfo.setFirstName("Sachin");
+		pinfo.setLastName("yaduvanshi");
+		pinfo.setJourneyDate("16/04/2006");
+		pinfo.setFrom("Gurugram");
+		pinfo.setTo("Goa");
+		pinfo.setTrainNumber("KKExpress-123");
+
+		// Creating the header object for holding details
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Accept", "application/json");
+		headers.add("Content-Type", "application/json");
+
+		// Creating an entity object to send the request
+		HttpEntity<PassengerInfo> reqEntity = new HttpEntity<>(pinfo, headers);
+
+		// Creating a RestTemplate object
+		RestTemplate rt = new RestTemplate();
+
+		System.out.println("*********SENDING THE REQUEST **************");
+		
+		// Sending the request
+		ResponseEntity<Ticket> ticketEntity = rt.exchange(
+						REST_ENDPOINT_URL, 
+						HttpMethod.POST, 
+						reqEntity, 
+						Ticket.class);
+
+		System.out.println("*********GETTING THE RESPONSE **************");
+		System.out.println(ticketEntity.getStatusCode().value());
+
+		// getting the response object[Ticket object]
+		System.out.println(ticketEntity.getBody());
+
+	}
+
+}
